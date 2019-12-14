868a00797c7e254120964ca968834966982bfae2
doyel saha <51129030+doyelsaha510@users.noreply.github.com>
2019-12-14 17:31:45 +0530

    Remove Items Using splice()

---
+++ BasicDs5.js
@@ -0,0 +1,7 @@
+//Remove Items Using splice()
+// splice() allows us to do just that: remove any number of consecutive elements from anywhere in an array.
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// only change code below this line
+arr.splice(1,4);
+// only change code above this line
+console.log(arr);
