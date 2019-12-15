5a383fce54cd59f2c319f9345bf37a1db4b17ba6
doyel saha <51129030+doyelsaha510@users.noreply.github.com>
2019-12-16 00:46:46 +0530

    Boo who

---
+++ BasicAlgo10.js
@@ -0,0 +1,13 @@
+//Boo who
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  
+  if(typeof bool==="boolean")
+  {
+      return true;
+  }
+    
+    return false;
+}
+
+booWho(null);
