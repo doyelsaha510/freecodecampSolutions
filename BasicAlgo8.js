0d4717d0315abc1f8ac887deca4cee551442d285
doyel saha <51129030+doyelsaha510@users.noreply.github.com>
2019-12-16 00:26:31 +0530

    Truncate a String

---
+++ BasicAlgo8.js
@@ -0,0 +1,11 @@
+//Truncate a String
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  if(str.length<=num)
+  {
+    return str;
+  }
+  return str.slice(0,num)+"...";
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
