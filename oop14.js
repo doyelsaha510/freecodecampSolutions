0c46ca40d18bc4db1ec3156003cf12a5ac60380b
doyel saha <51129030+doyelsaha510@users.noreply.github.com>
2019-12-18 06:31:13 +0530

    Set Constructor Property when Changing the Prototype

---
+++ oop14.js
@@ -0,0 +1,16 @@
+//Set the Constructor Property when Changing the Prototype
+function Dog(name) {
+  this.name = name;
+}
+
+// Modify the code below this line
+Dog.prototype = {
+  constructor:Dog,
+  numLegs: 4,
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
