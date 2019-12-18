bed2d83e690b5d69cf136138160df565b179b8c7
doyel saha <51129030+doyelsaha510@users.noreply.github.com>
2019-12-18 07:02:02 +0530

    Child's Prototype to an Instance of the Parent

---
+++ oop19.js
@@ -0,0 +1,18 @@
+//Child's Prototype to an Instance of the Parent
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+
+// Add your code below this line
+
+Dog.prototype=Object.create(Animal.prototype);
+
+let beagle = new Dog();
+beagle.eat();  // Should print "nom nom nom"
